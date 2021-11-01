5e49c26d7df3abb107488c90ea9f83bd690a508b
Bernardo Olisan <bernardoOlisan@gmail.com>
2021-10-31 22:01:48 -0600

    Add files via upload

---
+++ client.py
@@ -0,0 +1,28 @@
+import socket
+import tqdm
+import os
+
+SEPARATOR = '<SEPARATOR>'
+BUFFER_SIZE = 4096
+
+s = socket.socket()
+host = '192.168.1.93'
+port = 5001
+print(f'[+] Conneting to {host}:{port}')
+s.connect((host, port))
+print('[+] Connected to ', host)
+
+filename = input("File to Transfer : ")
+filesize = os.path.getsize(filename)
+s.send(f'{filename}{SEPARATOR}{filesize}'.encode())
+
+progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+with open(filename, "rb") as f:
+    while True:
+        bytes_read = f.read(BUFFER_SIZE)
+        if not bytes_read:
+            break
+        s.sendall(bytes_read)
+        progress.update(len(bytes_read))
+     
+s.close()
---
+++ server.py
@@ -0,0 +1,38 @@
+import socket
+import os
+import tqdm
+
+SERVER_HOST = '0.0.0.0' # All ipv4 
+SERVER_PORT = 5001 # port
+BUFFER_SIZE = 4096
+SEPARATOR = '<SEPARATOR>'
+
+s = socket.socket()
+s.bind((SERVER_HOST, SERVER_PORT))
+s.listen(10) # Connections to accept, 10 is the limit, only 10 ppl
+
+print(f'[*] Listening as {SERVER_HOST}:{SERVER_PORT}')
+print("Waiting for the client to connect...")
+
+client_socket, address = s.accept()
+print(f'[+] {address} is connected.')
+
+received = client_socket.recv(BUFFER_SIZE).decode()
+
+filename, filesize = received.split(SEPARATOR)
+filename = os.path.basename(filename)
+filesize = int(filesize)
+
+
+progress = tqdm.tqdm(range(filesize), f'Receiving {filename}', unit='B', unit_scale=True, unit_divisor=1024)
+
+with open(filename, 'wb') as f:
+    while True:
+        bytes_read = client_socket.recv(BUFFER_SIZE)
+        if not bytes_read:
+            break
+        f.write(bytes_read)
+        progress.update(len(bytes_read))
+
+client_socket.close()
+s.close()
